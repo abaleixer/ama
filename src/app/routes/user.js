466b77df825583146a279576940763fbd9ebd7b6
Aleixer Alvarado Bernal <aleixer@proax.org>
2018-09-26 15:23:46 -0500

    node-mysql

--- src/app/routes/user.js
+++ src/app/routes/user.js
@@ -1,5 +1,42 @@
+const dbConnection = require('../../config/dbConnection');
+
 module.exports = app => {
-    app.get('/', (req, res) => {
-        res.send('Hola usuario 3000');
+ 
+    // const connection = dbConnection();
+     people = ['geddy', 'neil', 'alex'],
+    app.get('/home', (req, res) => {
+        res.render('home/home', {
+            Persona: people
+        });
+
+        // connection.query('SELECT * FROM Personas', (err, result) => {
+        //     res.render('home/home', {
+        //         personas: result
+        //     });
+        // });
     });
+
+
+
+    // app.post('/AddPersona', (req, res) => {
+    //     const { title, news } = req.body;
+    //     connection.query('INSERT INTO news SET ? ',
+    //       {
+    //         title,
+    //         news
+    //       } , (err, result) => {
+    //       res.redirect('/news');
+    //     });
+    //   });
+    app.post('/AddPersona', (req, res) => {
+        const { title, news } = req.body;
+        people.push(title);
+    console.log(title,news);
+
+    // prueba auto renderando
+    // res.render('home/home', {
+    //     Persona: people
+    // });
+    // res.redirect('/home');
+      });
 }
